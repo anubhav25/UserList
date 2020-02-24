a19fc17c7a7bce978e4c43e24290fbc5685bd6ca
anubhav <anubhav>
2020-02-25 00:58:53 +0530

    Updates

---
+++ precache-manifest.1d7774bb3cf871033f2575b3bff32dd3.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "412baf12e9eedc24dc29a95ea7609dc5",
+    "url": "/UserList/index.html"
+  },
+  {
+    "revision": "f2f48b3851d954c4386b",
+    "url": "/UserList/static/css/main.a770ab9c.chunk.css"
+  },
+  {
+    "revision": "e5a8a92e70bda0d69c1b",
+    "url": "/UserList/static/js/2.689642bb.chunk.js"
+  },
+  {
+    "revision": "928d7b5eb39d16fe9a880722c974b51e",
+    "url": "/UserList/static/js/2.689642bb.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f2f48b3851d954c4386b",
+    "url": "/UserList/static/js/main.aead5718.chunk.js"
+  },
+  {
+    "revision": "0d82d2f292d37cab0a89",
+    "url": "/UserList/static/js/runtime-main.74693816.js"
+  }
+]);
