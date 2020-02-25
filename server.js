795498f43da823ecb0732c19ffe5f0d9ca17e660
anubhav <anubhav>
2020-02-25 17:46:07 +0530

    fixed port

--- server.js
+++ server.js
@@ -1,10 +1,11 @@
 const express = require("express");
 const path = require("path");
 const app = express();
+const port = process.env.PORT || 5000;
 app.use(express.static(path.join(__dirname, "build")));
 app.get("*", function(req, res) {
   res.sendFile(path.join(__dirname, "build", "index.html"));
 });
-app.listen(80, () => {
-  console.log("listening on port 80");
+app.listen(port, () => {
+  console.log("listening on port ", port);
 });
